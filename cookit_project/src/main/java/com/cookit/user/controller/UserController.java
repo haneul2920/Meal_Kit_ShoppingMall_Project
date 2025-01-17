5c7661132e0f3775f2a5a0767442e210b0f698c1
haneul2920 <kge2920@gmail.com>
2025-01-17 10:40:49 +0900

    cookit_project

---
+++ cookit_project/src/main/java/com/cookit/user/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.cookit.user.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+public interface UserController {
+	public ModelAndView login(@RequestParam Map<String, String> loginMap,HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ResponseEntity  addMember(@ModelAttribute("user") UserVO user,
+            HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ResponseEntity   overlapped(@RequestParam("id") String id,HttpServletRequest request, HttpServletResponse response) throws Exception;
+
+}
